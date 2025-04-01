78c945469e1da3d1fe142db905d81155a64b7a33
liangling0628 <lingliang1100@gmail.com>
2025-04-01 14:48:08 +0800

    feat: bkui-vue组件升级及新版trace开发 # Reviewed, transaction id: 36991

--- bkmonitor/webpack/src/trace/pages/main/span-details.tsx
+++ bkmonitor/webpack/src/trace/pages/main/span-details.tsx
@@ -821,7 +821,9 @@ export default defineComponent({
               <span class='left'>
                 <span
                   class='left-title'
-                  v-bk-overflow-tips
+                  v-overflow-title={{
+                    type: 'tips',
+                  }}
                 >
                   {item.label}
                 </span>
